838e4e4ea7c4244bb694d4c6ea0a999895cdd4a2
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-06-18 11:33:27 +0200

    scroll to top

--- js/app.js
+++ js/app.js
@@ -5,3 +5,20 @@ menu.addEventListener(`click`, function() {
     menu.classList.toggle(`is-active`);
     menulinks.classList.toggle(`active`);
 });
+
+const el = document.getElementById('jsScroll');
+
+window.addEventListener('scroll', function (e) {
+        if (window.scrollY > 200) {
+            el.classList.add('visible');
+        } else {
+            el.classList.remove('visible');
+        }
+    });
+
+function scrollToTop() {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+    });
+}
